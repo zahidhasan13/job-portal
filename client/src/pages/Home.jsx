95834bcd5efadb84fa0c3f8f750524ecc268aceb
zahidhasan13 <zahidhasanofficial13@gmail.com>
2025-03-17 16:17:02 +0600

    category section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/HeroSection';
+import JobCategorySection from '@/components/JobCategorySection';
 import React from 'react';
 
 const Home = () => {
     return (
         <>
             <HeroSection/>
+            <JobCategorySection/>
         </>
     );
 };
